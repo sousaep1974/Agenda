f6e6c77619833f341802c91778009a32fd817ccf
Edivan Sousa <sousaep1974@gmail.com>
2022-10-30 03:05:55 -0300

    Finish (closed)

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.CONNECTIONSTRING)
+  .connect(process.env.CONNECTIONSTRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => {
-    console.log("BASE DE DADOS (MongoDB) CONECTADA COM SUCESSO.");
-    app.emit("Pronto");
+    app.emit("pronto");
   })
   .catch((e) => console.log(e));
 const session = require("express-session");
@@ -14,7 +17,7 @@ const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const routes = require("./routes");
 const path = require("path");
-const helmet = require("helmet");
+// const helmet = require('helmet'); // helmet começou a causar problemas no localhost por conta da falta de SSL
 const csrf = require("csurf");
 const {
   middlewareGlobal,
@@ -22,13 +25,14 @@ const {
   csrfMiddleware,
 } = require("./src/middlewares/middleware");
 
-app.use(helmet());
+// app.use(helmet()); // helmet começou a causar problemas no localhost por conta da falta de SSL
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
 
 const sessionOptions = session({
-  secret: "dghfghfngçij()",
+  secret: "akasdfj0út23453456+54qt23qv  qwf qwer qwer qewr asdasdasda a6()",
   store: MongoStore.create({ mongoUrl: process.env.CONNECTIONSTRING }),
   resave: false,
   saveUninitialized: false,
@@ -37,7 +41,6 @@ const sessionOptions = session({
     httpOnly: true,
   },
 });
-
 app.use(sessionOptions);
 app.use(flash());
 
@@ -45,13 +48,13 @@ app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
 app.use(csrf());
-//Nossos próprios middlewares
+// Nossos próprios middlewares
 app.use(middlewareGlobal);
 app.use(checkCsrfError);
 app.use(csrfMiddleware);
 app.use(routes);
 
-app.on("Pronto", () => {
+app.on("pronto", () => {
   app.listen(3000, () => {
     console.log("Acessar http://localhost:3000");
     console.log("Servidor executando na porta 3000");
